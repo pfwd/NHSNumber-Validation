e94ea4bbffd192fd4254906792520679893ccf3d
Peter Fisher <work@peterfisher.me.uk>
2011-08-23 00:10:27 +0100

    Adding files

---
+++ PHP/NHSValidation.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of NHSValidation
+ *
+ * @author Pete
+ */
+class NHSValidation
+{
+    //put your code here
+}
+
+?>
